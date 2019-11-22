0e834f4e157008136db8161edda79fa9191317e7
Zac Smith <zrs3141592@gmail.com>
2019-11-21 20:14:57 -0700

    update knexfile filename and useNullAsDefault

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,9 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./dev.sqlite3"
-    }
+      filename: "./data/recipes.db3"
+    },
+    useNullAsDefault: true
   },
   migrations: {
     directory: "./data/migrations"
